6e5df55583509457e60c44c9a886a84bd546f669
wtlin1228 <wtlin1228@gmail.com>
2022-05-10 12:36:28 +0800

    try to collect coverage from interaction testing

--- test-runner-jest.config.js
+++ test-runner-jest.config.js
@@ -1,9 +1,21 @@
-const { getJestConfig } = require('@storybook/test-runner');
+const { getJestConfig } = require("@storybook/test-runner");
+
+const defaultJestConfig = getJestConfig();
 
 module.exports = {
   // The default configuration comes from @storybook/test-runner
-  ...getJestConfig(),
+  ...defaultJestConfig,
   /** Add your own overrides below
    * @see https://jestjs.io/docs/configuration
    */
-}
+  // collectCoverage: true,
+  // collectCoverageFrom: ["**/*.jsx", "!**/node_modules/**", "!**/vendor/**"],
+  testEnvironmentOptions: {
+    ...defaultJestConfig.testEnvironmentOptions,
+    "jest-playwright": {
+      ...defaultJestConfig.testEnvironmentOptions["jest-playwright"],
+      // Options...
+      collectCoverage: true,
+    },
+  },
+};
